f3fa587e976dc1a4cbc38110e66d152c1a65b1d0
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-01-26 22:19:13 +0600

    add helpers

--- TLabs.DotnetHelpers/Extensions/DateTimeOffsetExtensions.cs
+++ TLabs.DotnetHelpers/Extensions/DateTimeOffsetExtensions.cs
@@ -4,11 +4,6 @@ namespace TLabs.DotnetHelpers
 {
     public static class DateTimeOffsetExtensions
     {
-        /// <summary>
-        /// Get date with 00:00 UTC +0
-        /// </summary>
-        public static DateTimeOffset RemoveTimePart(this DateTimeOffset date)
-            => new DateTimeOffset(date.Date, TimeSpan.Zero);
 
         /// <summary>
         /// Get string that will be parsed to DateTimeOffset correctly
@@ -22,6 +17,22 @@ namespace TLabs.DotnetHelpers
         {
             TimeSpan t = date.UtcDateTime - new DateTime(1970, 1, 1, 0, 0, 0);
             return (int)t.TotalSeconds;
+        }
+
+        /// <summary>
+        /// Get date with 00:00 UTC +0
+        /// </summary>
+        public static DateTimeOffset RemoveTimePart(this DateTimeOffset date)
+            => new DateTimeOffset(date.Date, TimeSpan.Zero);
+
+        public static DateTimeOffset GetStartOfWeek(this DateTimeOffset date)
+        {
+            int delta = DayOfWeek.Monday - date.DayOfWeek;
+            if (delta == 1) // Sunday is first in DayOfWeek
+            {
+                delta -= 7;
+            }
+            return date.AddDays(delta);
         }
     }
 }
---
+++ TLabs.DotnetHelpers/Helpers/PredicateBuilder.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TLabs.DotnetHelpers.Helpers
+{
+    public static class PredicateBuilder
+    {
+        /// <summary>
+        /// Start new expression
+        /// </summary>
+        /// <param name="initialValue">false - for OR-chains, true - for AND-chains</param>
+        public static Expression<Func<T, bool>> Init<T>(bool initialValue)
+        {
+            return f => initialValue;
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1,
+                                                          Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+              (Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+                                                           Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+              (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+    }
+}
