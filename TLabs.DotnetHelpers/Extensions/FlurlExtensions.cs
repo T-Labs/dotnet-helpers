7bc3f7fabb1672c4722a3ddee510fd2b3e818a4d
Artur Teregulov <info@teregulov.com>
2021-06-06 00:18:42 +0300

    duplicate InitFlurl

--- TLabs.DotnetHelpers/Extensions/FlurlExtensions.cs
+++ TLabs.DotnetHelpers/Extensions/FlurlExtensions.cs
@@ -48,6 +48,32 @@ namespace TLabs.DotnetHelpers
             });
         }
 
+        public static void InitFlurl(this IServiceCollection services, string gatewayUrl)
+        {
+            _gatewayUrl = gatewayUrl;
+
+            FlurlHttp.Configure(settings =>
+            {
+                settings.JsonSerializer = new NewtonsoftJsonSerializer(new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                });
+
+                // log info of error request
+                settings.OnErrorAsync = async (FlurlCall call) =>
+                {
+                    const int maxLength = 500;
+                    string body = call.RequestBody?.Substring(0, Math.Min(call.RequestBody?.Length ?? 0, maxLength));
+                    string responseContent = null;
+                    if (call.HttpResponseMessage?.Content != null)
+                    {
+                        responseContent = await call.HttpResponseMessage.Content.ReadAsStringAsync() ?? "";
+                        responseContent = responseContent.Substring(0, Math.Min(responseContent.Length, maxLength));
+                    }
+                };
+            });
+        }
+
         // Extend POST and PUT requests to specify response type in a single call
 
         public static async Task<T> PostJsonAsync<T>(this IFlurlRequest request, object data,
