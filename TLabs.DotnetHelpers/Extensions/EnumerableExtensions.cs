2aba5d184af74f8cbaf5d3f941078c1e11ad63fb
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-11-01 19:41:04 +0600

    add EnumerableExtension

--- TLabs.DotnetHelpers/Extensions/EnumerableExtensions.cs
+++ TLabs.DotnetHelpers/Extensions/EnumerableExtensions.cs
@@ -6,11 +6,15 @@ namespace TLabs.DotnetHelpers
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<T> UnionSafe<T>(
-            this IEnumerable<T> source1, IEnumerable<T> source2)
+        public static IEnumerable<T> UnionSafe<T>(this IEnumerable<T> source1, IEnumerable<T> source2)
         {
             return source1 != null ?
                 (source2 != null ? source1.Union(source2) : source1) : source2;
+        }
+
+        public static string ToStrings<T>(this IEnumerable<T> source, string separator = "\n ")
+        {
+            return string.Join(separator, source.Select(_ => _.ToString());
         }
     }
 }
