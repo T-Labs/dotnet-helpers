01e4789ad57700cfc033c4e31decce7a68e4f9a3
Vitaliy <vitaliy.lebedev7@gmail.com>
2023-07-05 00:59:38 +0600

    add CodesGenerator helper

---
+++ TLabs.DotnetHelpers/Helpers/CodesGenerator.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace TLabs.DotnetHelpers.Helpers
+{
+    public static class CodesGenerator
+    {
+        /// <summary>Create a string with random digits and/or letters</summary>
+        public static string Create(int length, bool useDigits = false, bool useUpperLetters = false, bool useLowerLetters = false)
+        {
+            if (length <= 0)
+                throw new ArgumentException("length must be greater than 0");
+            string chars = "";
+            if (useDigits)
+                chars += "23456789"; // 0 and 1 are not used because they are similar to O and l
+            if (useUpperLetters)
+                chars += "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+            if (useLowerLetters)
+                chars += "abcdefghijkmnpqrstuvwxyz";
+            if (chars.NotHasValue())
+                throw new ArgumentException("At least one of useDigits, useUpperLetters, useLowerLetters must be true");
+            var rand = new Random();
+            var result = string.Join("", Enumerable.Range(0, length).Select(i => chars[rand.Next(chars.Length)]));
+            return result;
+        }
+    }
+}
