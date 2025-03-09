ad7e3a8c9509922f6d3d8b073d9b7070c3e496c4
Vitaliy <vitaliy.lebedev7@gmail.com>
2025-03-09 23:56:50 +0500

    update GuidExtensions

--- TLabs.DotnetHelpers/Extensions/GuidExtensions.cs
+++ TLabs.DotnetHelpers/Extensions/GuidExtensions.cs
@@ -5,6 +5,17 @@ namespace TLabs.DotnetHelpers
 {
     public static class GuidExtensions
     {
+
+        static int[] byteOrder = { 15, 14, 13, 12, 11, 10, 9, 8, 6, 7, 4, 5, 0, 1, 2, 3 };
+
+        /// <summary>Increase guid value by 1</summary>
+        public static Guid Increment(this Guid guid)
+        {
+            var bytes = guid.ToByteArray();
+            var canIncrement = byteOrder.Any(i => ++bytes[i] != 0);
+            return new Guid(canIncrement ? bytes : new byte[16]);
+        }
+
         /// <summary>Create new guid from 2 existing guids</summary>
         public static Guid Combine(this Guid guid1, Guid guid2)
         {
