9eecd069e1a634ef3e3a1ec8d8705b4f92b5b88f
Vitaliy <vitaliy.lebedev7@gmail.com>
2025-03-17 14:46:30 +0500

    add EnumExtensions

---
+++ TLabs.DotnetHelpers/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace TLabs.DotnetHelpers
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                .GetName();
+        }
+    }
+}
